877dc6dba737df340730ca742b7a22d4a51a6b61
Bobby Smith <bobbysmith24@gmail.com>
2014-09-02 10:18:24 -0500

    iphone tmplates

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902045256) do
+ActiveRecord::Schema.define(version: 20140902045759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flights", force: true do |t|
+    t.string   "origin"
+    t.string   "destination"
+    t.datetime "date"
+    t.string   "class"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
